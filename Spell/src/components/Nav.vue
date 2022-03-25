9743708c802f76c4319c1374170c33e7ddf45c9a
Apolline <achenais@enssat.fr>
2022-03-25 13:51:44 +0100

    feat(nav): new style

--- Spell/src/components/Nav.vue
+++ Spell/src/components/Nav.vue
@@ -1,13 +1,45 @@
 <template>
   <nav>
-    NAV-BAR ICI
-  </nav>
+        <img src="../assets/logo.png" height="40vh">
+        <a href="#" class="name">Sortilege</a>
+        <ul>
+            <li><a href="#">Configuration</a></li>
+            <li><a href="Resultat.html">Recherche</a></li>
+            <li><a href="Statistiques.html">Statistiques</a></li>
+        </ul>
+    </nav>
 </template>
 
-<style scoped>
-nav {
-  color: var(--text-primary);
-  background-color: bisque;
-  padding: 1rem;
-}
+<style lang="css" scoped>
+   nav{
+       display:flex;
+       flex-direction: row;
+       justify-content: space-between;
+       height: 10vh;
+       margin:0;
+       position: sticky;
+       width:100wv;
+       align-items: center;
+       background-color: var(--background-secondary-color);
+   }
+   nav ul{
+        display: flex;
+        flex-direction: row;
+        width:90vw;
+        justify-content: flex-end;
+        margin:0;
+        list-style: none;
+    }
+
+    nav ul li{
+        margin : 0 20px;
+        font-size:  var(--nav-size);
+    }
+    .name{
+        font-family: var(--font-stat);
+        font-size: 2rem;
+        padding:5px;
+        color: var(--primary-color);
+    }
+
 </style>
