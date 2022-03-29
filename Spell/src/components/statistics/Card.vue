948fc9f04a7480306378e311ac1d97d1fc9e827f
Apolline <achenais@enssat.fr>
2022-03-29 16:57:45 +0200

    css suite 2

---
+++ Spell/src/components/statistics/Card.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="stat-item">
+    <p class="stat-nb">CHIFFRE</p>
+    <p> BLABLA</p>
+  </div>
+</template>
+
+<style lang="css" scoped>
+.stat-item{
+  width:15vw;
+  background-color: var(--background-secondary-color);
+  text-align: center;
+  padding:5px;
+}
+.stat-nb{
+  font-size:4.5rem;
+  margin:0;
+  padding:0;
+  font-family: var(--font-stat);
+  color:var(--primary-color);
+}
+p{
+  margin:0;
+}
+</style>
